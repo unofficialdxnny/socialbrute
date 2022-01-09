a321794f2b75ea089dc5c98fdb313c4a1269d459
unofficialdxnny <82535503+unofficialdxnny@users.noreply.github.com>
2022-01-09 15:41:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,15 @@ os.system('cls')
 
 
 os.system('color 0A') 
+
+
+
+def type(text):
+  words = text
+  for char in words:
+    sleep(0.2)
+    sys.stdout.write(char)
+    sys.stdout.flush()
+type("Welcome To SocialBrute \n")
+
+print("\n")
