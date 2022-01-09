c29a3a514c68082b67775517ddffbdc25391fb44
unofficialdxnny <82535503+unofficialdxnny@users.noreply.github.com>
2022-01-09 20:39:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 
+## Python bruteforce attacker
+## By @unofficialdxnny
+
+
+
+### Modules that will be used
+import getpass
+from datetime import date 
+from colorama import Fore, Back, Style
+import sys
+from time import sleep
+import keyboard
+import pyautogui as pag
+import os
