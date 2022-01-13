1b72c0ae01299f808fc2e75ceed88802af7498c5
unofficialdxnny <82535503+unofficialdxnny@users.noreply.github.com>
2022-01-13 10:38:49 +0000

    Update pg.py

--- pg.py
+++ pg.py
@@ -1,5 +1,4 @@
-## this is just my test file 
-
+## this is just my test file  if you want to test some code changes try them here than create a pull request with the changes
 
 with open("passwords.txt", 'r', encoding='UTF-8') as file:
     while (line := file.readline().rstrip()):
